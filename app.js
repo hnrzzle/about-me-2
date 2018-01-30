1b499c59bb41e52a559f6e6254a3b78ee92d07c7
hnrzzle <henrique.j.oliveira1@gmail.com>
2018-01-30 14:43:15 -0800

    Question 1 done and working

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
-'use script';
+'use strict';
+
 //asking questions
 const name = prompt('What is your name?');
-const state = prompt('What state do you live in?');
-const petCount = prompt('How many pets do you have?');
-const musicianResult = prompt('How many instruments do you play?');
+console.log('Their name is ' + name);
 
-//alert message
-alert('Hello ' + name + ' from ' + state + '. I see you have ' + petCount + ' pets, I love animals! It\'s interesting you play ' + musicianResult + ' instruments, I play the guitar!');
+const petOwner = prompt('Nice to meet you ' + name + ' Do you have any pets?').toLowerCase();
 
-//what's their name?
-console.log('their name is ' + name);
-//where do they live?
-console.log(name + ' lives in ' + state);
-//how many pets do they have?
-console.log(name + ' has ' + petCount + ' pets');
-//how many instruments do they play?
-console.log(name + ' plays ' + musicianResult + ' instruments');
+console.log('Are they a pet owner?: ' + petOwner);
+
+if (petOwner === ('yes' || 'y')) {
+    alert('yay, I love animals!');
+} else if (petOwner === ('no' || 'n')) {
+    alert('Aw, you should get one if you want one!');
+} else {
+    alert('Please answer with yes or no!');
+}
