4e0fd51e72c9f8b1543fcbf5805887b2f490ac0d
hnrzzle <henrique.j.oliveira1@gmail.com>
2018-01-30 15:20:00 -0800

    completed five questions, working

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const petOwner = prompt('Nice to meet you ' + name + '! Do you have any pets?').
 console.log('Are they a pet owner?: ' + petOwner);
 
 if (petOwner === ('yes' || 'y')) {
-    alert('yay, I love animals!');
+    alert('Yay, I love animals!');
 } else if (petOwner === ('no' || 'n')) {
     alert('Aw, you should get one if you want one!');
 } else {
@@ -26,7 +26,7 @@ console.log('Is ' + name + ' an Oregon resident?: ' + oregonResident);
 if (oregonResident === ('yes' || 'y')) {
     alert('Oh, neat, I live in Washington!');
 } else if (oregonResident === ('no' || 'n')) {
-    alert('Oh interesting, I live in Washington');
+    alert('Ah, a fellow out-of-stater; I live in Washington');
 } else {
     alert('Please answer with yes or no!');
 }
@@ -38,6 +38,28 @@ if (musician === ('yes' || 'y')) {
     alert('Awesome, I play guitar!');
 } else if (musician === ('no' || 'n')) {
     alert('You should try to learn one, I play guitar!');
+} else {
+    alert('Please answer with yes or no!');
+}
+
+const tacoLover = prompt('Do you like tacos ' + name + '?').toLowerCase();
+console.log('Is ' + name + ' a taco fan?: ' + tacoLover);
+
+if (tacoLover === ('yes' || 'y')) {
+    alert('Me too! I love tacos!');
+} else if (tacoLover === ('no' || 'n')) {
+    alert(':( Tacos are the best!');
+} else {
+    alert('Please answer with yes or no!');
+}
+
+const likesVideoGames = prompt('Do you like video games ' + name + '?').toLowerCase();
+console.log('Does ' + name + ' like video games?: ' + likesVideoGames);
+
+if (likesVideoGames === ('yes' || 'y')) {
+    alert('Right on, me too! My favorite game right now is Overwatch.');
+} else if (likesVideoGames === ('no' || 'n')) {
+    alert('Aw, you should give them a try, they\'re very rewarding!');
 } else {
     alert('Please answer with yes or no!');
 }
